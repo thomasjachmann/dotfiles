503c30d2c71d0492c4ec737cd9efcf4bce9c8391
Thomas Jachmann <self@thomasjachmann.com>
2013-09-16 23:01:13 +0200

    Let homebrew modify the path more early
    Chruby depends on brew being in the path.

---
+++ homebrew/.zshrc.d/_homebrew.zsh
@@ -0,0 +1,5 @@
+# extend path for homebrew
+PATH=/usr/local/bin:/usr/local/sbin:$PATH
+
+# and for brew's npm
+PATH=/usr/local/share/npm/bin:$PATH
--- homebrew/.zshrc.d/homebrew.zsh
+++ homebrew/.zshrc.d/homebrew.zsh
@@ -1,14 +1,3 @@
-PATH=/usr/local/bin:/usr/local/sbin:$PATH
-
-# and for npm
-PATH=/usr/local/share/npm/bin:$PATH
-
-# source all files in /usr/local/etc/bash_completion.d (sorted alphabetically, no . files)
-# these are homebrew's bash completion files
-# for FILE in /usr/local/etc/bash_completion.d/*; do
-#   source $FILE
-# done
-
 function brew-update() {
   brew update
   echo "\033[1;34m==>\033[1;39m Outdated Formulae\033[0m"
