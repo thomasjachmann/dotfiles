f67854bd699c8332a3b09cd64dcd6a5b84596e8f
Thomas Jachmann <self@thomasjachmann.com>
2013-09-16 23:07:04 +0200

    Simply chruby version determination
    chruby now highlights the specific version only, not the one called eg
    when using a symlink.

--- chruby/.zshrc.d/chruby.zsh
+++ chruby/.zshrc.d/chruby.zsh
@@ -2,13 +2,8 @@ source `brew --prefix chruby`/share/chruby/chruby.sh
 source `brew --prefix chruby`/share/chruby/auto.sh
 
 function chruby-version() {
-  # ruby --version | cut -d " " -f2
-  local ruby_version=`chruby | grep '*' | sed 's/ \* //'`
-  local specific_version=`readlink ~/.rubies/$ruby_version`
-  if [[ $specific_version != "" ]]; then
-    ruby_version="$ruby_version ($specific_version)"
-  fi
-  echo $ruby_version
+  local version=`chruby | grep '*' | sed 's/ \* //'`
+  echo $version
 }
 
 local light=7 # see prompt.zsh
