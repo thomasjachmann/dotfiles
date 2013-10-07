dbd435dd9de1025973f273e88bb770fa88ca94b7
Thomas Jachmann <self@thomasjachmann.com>
2013-10-07 17:43:14 +0200

    blau changes

--- customer-blau/.zshrc.d/customer-blau.zsh
+++ customer-blau/.zshrc.d/customer-blau.zsh
@@ -1,5 +1,15 @@
-function cpoisi() {
+export DYLD_LIBRARY_PATH="/usr/local/oracle/instantclient_10_2"
+export SQLPATH="/usr/local/oracle/instantclient_10_2"
+export TNS_ADMIN="/usr/local/oracle/network/admin"
+export NLS_LANG="AMERICAN_AMERICA.UTF8"
+export PATH=$PATH:$DYLD_LIBRARY_PATH
+
+function bcd() {
+  cd ~/platform/ruby/apps/$1
+}
+
+function bcp() {
   local dir=`dirname $1`
-  mkdir -p ~/platform/ruby/apps/oisi2/$dir
-  cp -r ~/platform/ruby/apps/oisi/$1 ~/platform/ruby/apps/oisi2/$dir
+  mkdir -p "$HOME/platform/ruby/apps/oisi2/$dir"
+  cp -r "$HOME/platform/ruby/apps/oisi/$1" "$HOME/platform/ruby/apps/oisi2/$dir"
 }
