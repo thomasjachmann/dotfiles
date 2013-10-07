52570a44330f60dcc6663ceceaa0745e70699336
Thomas Jachmann <self@thomasjachmann.com>
2013-10-07 12:41:05 +0200

    Organize path extension better
    _ files are there for path extension

--- zsh/.zshrc.d/_defaults.zsh
+++ zsh/.zshrc.d/_defaults.zsh
@@ -1,9 +1,6 @@
 # for tar: exclude ._ files (Mac OS X extended attributes)
 COPYFILE_DISABLE=true
 
-# for local executables
-PATH=~/bin:$PATH
-
 # for aliases/functions - print out command before executing it
 function say-and-execute() {
   echo "=> $*"
---
+++ zsh/.zshrc.d/_zsh_path.zsh
@@ -0,0 +1,5 @@
+# This file should be at the end of the _ files to ensure that
+# ~/bin is at the beginning of the path in order to override executables.
+
+# # for local executables
+PATH=~/bin:$PATH
