478062f05409de58b2834be825ebda17543c8849
Thomas Jachmann <self@thomasjachmann.com>
2013-10-18 22:28:41 +0200

    zsh: Use perl script to display nice ansi colors
    taken from:
    https://sourceforge.net/p/tmux/tmux-code/ci/master/tree/tools/256colors.pl

--- zsh/.zshrc.d/zsh.zsh
+++ zsh/.zshrc.d/zsh.zsh
@@ -6,7 +6,3 @@ function edit() {
     say-and-execute subl --new-window ~/.zshrc ~/.zshrc.d ~/.oh-my-zsh
   fi
 }
-
-# see http://www.commandlinefu.com/commands/view/6533/print-all-256-colors-for-testing-term-or-for-a-quick-reference
-# see http://www.calmar.ws/vim/256-xterm-24bit-rgb-color-chart.html
-alias print-colors='( x=`tput op` y=`printf %$((${COLUMNS}-6))s`;for i in {0..256};do o=00$i;echo -e ${o:${#o}-3:3} `tput setaf $i;tput setab $i`${y// /=}$x;done; )'
