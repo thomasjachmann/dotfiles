f22d9696a093d5ca626ce21b2842887c07d8ae07
Thomas Jachmann <self@thomasjachmann.com>
2014-05-05 12:47:14 +0200

    Switch from custom rails commands to oh-my-zsh's

--- ruby/.zshrc.d/ruby-rails.zsh
+++
@@ -1,7 +0,0 @@
-alias sc="bundle exec script/console"
-alias scs="bundle exec script/console -s"
-alias ss="bundle exec script/server"
-
-alias r="bundle exec rails"
-alias rs="bundle exec rails server"
-alias rc="bundle exec rails console"
