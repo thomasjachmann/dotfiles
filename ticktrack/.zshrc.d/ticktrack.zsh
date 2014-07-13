fec44098a22b33267866057f37bd37098a538cf4
Thomas Jachmann <self@thomasjachmann.com>
2014-07-13 20:57:59 +0200

    ticktrack: Add aliases to quickly run heroku cmds

---
+++ ticktrack/.zshrc.d/ticktrack.zsh
@@ -0,0 +1,3 @@
+alias ticktrack='say-and-execute heroku run rails console --app ticktrack'
+alias ticktrack-stats='say-and-execute heroku run rake ticktrack:stats --app ticktrack'
+alias ticktrack-stats-current='say-and-execute heroku run rake ticktrack:stats:current --app ticktrack'
