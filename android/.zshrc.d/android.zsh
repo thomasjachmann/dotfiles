afd3859b7988e57df97d393547f6c26d3755afaf
Thomas Jachmann <self@thomasjachmann.com>
2014-11-27 19:49:32 +0100

    android: Add stuff for android development

---
+++ android/.zshrc.d/android.zsh
@@ -0,0 +1,4 @@
+export PATH=${PATH}:/Applications/Android\ Studio.app/sdk/platform-tools:/Applications/Android\ Studio.app/sdk/tools
+export JAVA_HOME=$(/usr/libexec/java_home)
+export PATH=${JAVA_HOME}/bin:$PATH
+#export PATH=/usr/local/bin:$PATH
