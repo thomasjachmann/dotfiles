0873c07948e62d730944af68d52595a26c88a85d
Thomas Jachmann <self@thomasjachmann.com>
2014-11-27 19:40:11 +0100

    ruby: Modify bundler aliases for usage of bin/bundler

--- ruby/.zshrc.d/ruby-bundler.zsh
+++ ruby/.zshrc.d/ruby-bundler.zsh
@@ -1,9 +1,19 @@
+function _bundle_command () {
+  if [ -e "bin/bundle" ]; then
+    bin/bundle $@
+  else
+    bundle $@
+  fi
+}
+alias bundle='_bundle_command'
+#compdef _bundle_command=bundle
+
 function b() {
-  say-and-execute bundle $* | grep -v 'Using '
+  say-and-execute _bundle_command $* | grep -v 'Using '
 }
 alias bi="b install --path=.bundle/gems"
 alias bu="b update"
-alias bc="say-and-execute bundle console"
-alias be="say-and-execute bundle exec"
-alias bl="say-and-execute bundle list"
-alias bo="say-and-execute bundle open"
+alias bc="say-and-execute _bundle_command console"
+alias be="say-and-execute _bundle_command exec"
+alias bl="say-and-execute _bundle_command list"
+alias bo="say-and-execute _bundle_command open"
