f34c97ad09371bffb328900cb19a86bae0f49dae
Thomas Jachmann <self@thomasjachmann.com>
2015-02-15 23:24:35 +0100

    mjolnir: Allow window cycling
    This cycles the other way around than cmd-< but this is the only way
    that's doable easily since I just cycle the list the last window gets
    the focus and is inserted at the top of the list, the rest shifts one
    place down. If I'd focus the second window, it would become the first
    and the first would become the second and I'd never reach the third.
    This could only be solvable by storing the window list and cycling and
    then when would I throw the stored list away?
    
    The given fn is called on every switch. I just use it for Mail for now,
    and this is a bit annoying since the selected mailbox also changes when
    I'm switching away from the main window.

--- mjolnir/.mjolnir/apps.lua
+++ mjolnir/.mjolnir/apps.lua
@@ -1,10 +1,25 @@
 local apps = {}
 
+local appfinder = require "mjolnir.cmsj.appfinder"
 local application = require "mjolnir.application"
 local window = require "mjolnir.window"
 
 function apps.launch(app, fn)
   return function()
+    launchedapp = appfinder.app_from_name(app)
+    if (launchedapp) then
+      windows = launchedapp:visiblewindows()
+      if (#windows > 1) then
+        focused = window.focusedwindow()
+        for _, window in pairs(windows) do
+          if (window == focused) then
+            windows[#windows]:focus()
+            if (fn) then; fn(); end
+            return
+          end
+        end
+      end
+    end
     application.launchorfocus(app)
     if (fn) then; fn(); end
   end
