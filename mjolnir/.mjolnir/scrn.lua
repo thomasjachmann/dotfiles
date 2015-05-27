08848515aa2cdec7af4b9a44cd117433966e0ef7
Thomas Jachmann <self@thomasjachmann.com>
2015-05-27 22:58:35 +0200

    mjolnir: Don't stretch windows on screen change
    This made windows much too big when pushing them to the big external
    screen. Just keep the window as it is, move/downsize it if needed when
    pushing to a smaller screen, and that's it.

--- mjolnir/.mjolnir/scrn.lua
+++ mjolnir/.mjolnir/scrn.lua
@@ -4,24 +4,33 @@ local window = require "mjolnir.window"
 
 function scrn.movetonext()
   return function()
-    local win              = window.focusedwindow()
+    local win        = window.focusedwindow()
+    local win_frame  = win:frame()
 
-    -- taken from https://github.com/ledbettj/config-files/blob/master/mjolnir.lua
-    local old_screen       = win:screen()
-    local old_screen_frame = old_screen:frame()
-    local win_frame        = win:frame()
-    local new_screen       = old_screen:next()
-    local new_frame        = new_screen:frame()
-    local off_x_pct        = (win_frame.x - old_screen_frame.x) / (old_screen_frame.w)
-    local off_y_pct        = (win_frame.y - old_screen_frame.y) / (old_screen_frame.h)
+    local old_screen = win:screen()
+    local old_frame  = old_screen:frame()
+    local new_screen = old_screen:next()
+    local new_frame  = new_screen:frame()
 
-    local w_pct = win_frame.w / old_screen_frame.w
-    local h_pct = win_frame.h / old_screen_frame.h
+    local relative_x = win_frame.x - old_frame.x
+    local relative_y = win_frame.y - old_frame.y
 
-    win_frame.x = new_frame.x + (off_x_pct * new_frame.w)
-    win_frame.y = new_frame.y + (off_y_pct * new_frame.h)
-    win_frame.w = w_pct * new_frame.w
-    win_frame.h = h_pct * new_frame.h
+    if win_frame.w >= new_frame.w then
+      win_frame.x = new_frame.x
+      win_frame.w = new_frame.w
+    elseif relative_x + win_frame.w > new_frame.w then
+      win_frame.x = new_frame.x + new_frame.w - win_frame.w
+    else
+      win_frame.x = new_frame.x + relative_x
+    end
+    if win_frame.h >= new_frame.h then
+      win_frame.y = new_frame.y
+      win_frame.h = new_frame.h
+    elseif relative_y + win_frame.h > new_frame.h then
+      win_frame.y = new_frame.y + new_frame.h - win_frame.h
+    else
+      win_frame.y = new_frame.y + relative_y
+    end
 
     win:setframe(win_frame)
   end
