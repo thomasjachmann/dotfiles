35e5d5dc299e8b96cd05554510b7c1a475e83f6a
Thomas Jachmann <self@thomasjachmann.com>
2015-09-17 22:56:16 +0200

    vim: Switch to nvim

--- vim/.zshrc.d/vim.zsh
+++ vim/.zshrc.d/vim.zsh
@@ -1,10 +1,10 @@
 # export EDITOR='mvim -f --nomru -c "au VimLeave * !open -a iTerm"'
 # export EDITOR='mvim -f -c "au VimLeave * maca hide:"'
-export EDITOR='vim -f'
+export EDITOR='nvim -f'
 export BUNDLER_EDITOR='tabvim'
 
 # vanilla vim
-alias vvim='vim -u NONE -N'
+alias vvim='nvim -u NONE -N'
 
 function v() {
   if [ $# -eq 1 ]; then
