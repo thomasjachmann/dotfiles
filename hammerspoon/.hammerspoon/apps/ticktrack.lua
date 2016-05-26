b1a2a4b92795c7204f3e6bd3862fec80f1edae3e
Thomas Jachmann <self@thomasjachmann.com>
2016-05-26 13:09:49 +0200

    hammerspoon: Restyle ticktrack window
    * 20px wider
    * different border

--- hammerspoon/.hammerspoon/apps/ticktrack.lua
+++ hammerspoon/.hammerspoon/apps/ticktrack.lua
@@ -5,11 +5,12 @@ local webview = nil
 function ticktrack.toggle()
   if webview == nil then
     rect = hs.screen.primaryScreen():frame()
-    rect.x = rect.w - 350
-    rect.w = 350
+    rect.x = rect.w - 370
+    rect.w = 370
     webview = hs.webview.new(rect)
     webview:url("http://ticktrack.herokuapp.com")
     webview:allowTextEntry(true)
+    webview:windowStyle({"titled", "closable", "resizable"}) --, "fullSizeContentView"})
   end
 
   win = webview:asHSWindow()
