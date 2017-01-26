1f7c2a222652018ecfcbf1fbd705fc60d4ba65b1
Thomas Jachmann <self@thomasjachmann.com>
2017-01-26 12:06:05 +0100

    zsh: Fix some docs

--- zsh/.config/zsh/_zsh_path.zsh
+++ zsh/.config/zsh/_zsh_path.zsh
@@ -1,5 +1,6 @@
-# This file should be at the end of the _ files to ensure that
-# ~/bin is at the beginning of the path in order to override executables.
+# This file should be at the end of the _ files (they are sorted
+# alphabetically) to ensure that ~/bin is at the beginning of the path in order
+# to override executables.
 
-# # for local executables
+# include ~/bin for local executables
 PATH=~/bin:$PATH
