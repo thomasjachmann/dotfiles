af9ec238e95730ae7b26829044726a8c51c8df03
Thomas Jachmann <self@thomasjachmann.com>
2017-01-26 12:04:05 +0100

    oh-my-zsh: Add Brewfile and change installation
    Before, it was done with a submodule. Now, it's just cloned explicitly. The
    installation script mainly just checks the environment and modifies rc files
    which I don't want anyway. And I want to get rid of the git submodules. This
    comes with the cost that I can't pin the exact version of oh-my-zsh, but I can
    live with that.

--- oh-my-zsh/.config/zsh/oh-my-zsh.zsh
+++ oh-my-zsh/.config/zsh/oh-my-zsh.zsh
@@ -1,8 +1,8 @@
 # Path to your oh-my-zsh configuration.
-ZSH=$HOME/.oh-my-zsh
+ZSH=$HOME/.config/oh-my-zsh
 
 # Set name of the theme to load.
-# Look in ~/.oh-my-zsh/themes/
+# Look in ~/.config/oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
 ZSH_THEME=""
