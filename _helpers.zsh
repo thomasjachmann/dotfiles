af5b6fd998f9ccae7f7aa3f8ad8ce6215b3112f7
Thomas Jachmann <self@thomasjachmann.com>
2017-01-27 21:01:41 +0100

    hammerspoon/launchbar: Add login items
    This new helper adds a login item for an app and starts it immediately.
    This helps installing background apps w/o having to launch them manually
    and searching for the option to set them up to launch on login.

--- _helpers.zsh
+++ _helpers.zsh
@@ -2,3 +2,13 @@ function color  { echo -e "\033[$1m$2\033[0m"; }
 function green  { color "0;32" "$1"; }
 function yellow { color "1;33" "$1"; }
 function red    { color "0;31" "$1"; }
+
+# see http://hints.macworld.com/article.php?story=20111226075701552
+function loginitem {
+  local name=$1
+  local path=$2
+  if [ -n "$name" ] && [ -n "$path" ]; then
+    osascript -e "tell application \"System Events\" to make login item at end with properties {name:\"$name:\", path:\"$path\", hidden:false}"
+    open -a $path
+  fi
+}
