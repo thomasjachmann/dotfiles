b57b2240b45744c022638e54ebf4e415f57f4e05
Thomas Jachmann <self@thomasjachmann.com>
2017-03-07 21:02:38 +0100

    hammerspoon: Switch back to Thunderbird for email

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -56,8 +56,7 @@ bind.hyper({
   f = apps.launch("Finder"),
   g = apps.launch("GitX"),
   i = apps.launch("Textual"),              -- i for irc
-  --m = apps.launch("Thunderbird"),
-  m = apps.launch("Google Mail"),
+  m = apps.launch("Thunderbird"),
   n = apps.launch("Tweetbot"),             -- n for noise
   o = apps.launch("Opera"),
   --p = apps.launch("Trello"),               -- p for project management
