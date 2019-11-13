aba12c2ff9ac9943df515120ef0bb0fa3a114eec
Thomas Jachmann <self@thomasjachmann.com>
2019-11-14 00:04:05 +0100

    homebrew: Add some settings
    I found these in the man page under ENVIRONMENT.

--- homebrew/.config/zsh/_homebrew.zsh
+++ homebrew/.config/zsh/_homebrew.zsh
@@ -4,9 +4,20 @@ PATH=/usr/local/sbin:$PATH
 # and for brew's npm
 PATH=/usr/local/share/npm/bin:$PATH
 
+# prevent homebrew from sending data home
 HOMEBREW_NO_ANALYTICS=1
 
-# this is the token "Homebrew" in github account thomasjachmann to allow
-# unratelimited access for brew
-HOMEBREW_GITHUB_API_TOKEN=thisisnttheactualtokenireplaceditwiththissinceitactuallydoesntworkyet
-# Hm... doesn't work yet...
+# prevent homebrew to update on each install (I'd rather do this manually to save time)
+HOMEBREW_NO_AUTO_UPDATE=1
+
+# prevent homebrew to cleanup automatically (I'd rather do this manually to choose deliberately)
+HOMEBREW_NO_INSTALL_CLEANUP=1
+
+# don't search github issues etc. on `brew search`
+HOMEBREW_NO_GITHUB_API=1
+
+HOMEBREW_DISPLAY_INSTALL_TIMES=1
+
+# always use the internal tools instead of the system ones
+HOMEBREW_FORCE_VENDOR_RUBY=1
+HOMEBREW_FORCE_BREWED_GIT=1
