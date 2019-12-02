43e47228868073be2696165d86acce739abe5e1d
Thomas Jachmann <self@thomasjachmann.com>
2019-12-02 23:12:22 +0100

    WIP: homebrew: use export for variables - WHY?!

--- homebrew/.config/zsh/_homebrew.zsh
+++ homebrew/.config/zsh/_homebrew.zsh
@@ -5,19 +5,19 @@ PATH=/usr/local/sbin:$PATH
 PATH=/usr/local/share/npm/bin:$PATH
 
 # prevent homebrew from sending data home
-HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_ANALYTICS=1
 
 # prevent homebrew to update on each install (I'd rather do this manually to save time)
-HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_AUTO_UPDATE=1
 
 # prevent homebrew to cleanup automatically (I'd rather do this manually to choose deliberately)
-HOMEBREW_NO_INSTALL_CLEANUP=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
 
 # don't search github issues etc. on `brew search`
-HOMEBREW_NO_GITHUB_API=1
+export HOMEBREW_NO_GITHUB_API=1
 
-HOMEBREW_DISPLAY_INSTALL_TIMES=1
+export HOMEBREW_DISPLAY_INSTALL_TIMES=1
 
 # always use the internal tools instead of the system ones
-HOMEBREW_FORCE_VENDOR_RUBY=1
-HOMEBREW_FORCE_BREWED_GIT=1
+export HOMEBREW_FORCE_VENDOR_RUBY=1
+export HOMEBREW_FORCE_BREWED_GIT=1
