f978f9da6462232da1f852b89cab9cbe815bdcba
Thomas Jachmann <self@thomasjachmann.com>
2019-12-08 22:16:38 +0100

    ruby/oh-my-zsh: Remove rake/ruby plugins
    The things I did in the ruby package are basically what I used the
    oh-my-zsh plugins for.

--- ruby/.config/zsh/ruby-bundler.zsh
+++ ruby/.config/zsh/ruby-bundler.zsh
@@ -1,3 +1,35 @@
+# #compdef rails
+function _rails_command () {
+  if [ -e "bin/rails" ]; then
+    bin/rails $@
+  elif [ -e "script/rails" ]; then
+    ruby script/rails $@
+  elif [ -e "script/server" ]; then
+    ruby script/$@
+  else
+    command rails $@
+  fi
+}
+alias rails='_rails_command'
+# compdef _rails_command=rails
+
+alias rc="_rails_command c"
+alias rs="_rails_command s"
+
+# #compdef rake
+function _rake_command () {
+  if [ -e "bin/rake" ]; then
+    bin/rake $@
+  elif type bundle &> /dev/null && [ -e "Gemfile" ]; then
+    bundle exec rake $@
+  else
+    command rake $@
+  fi
+}
+alias rake='noglob _rake_command'
+# compdef _rake_command=rake
+
+# #compdef bundle
 function _bundle_command () {
   if [ -e "bin/bundle" ]; then
     bin/bundle $@
@@ -6,7 +38,7 @@ function _bundle_command () {
   fi
 }
 alias bundle='_bundle_command'
-#compdef _bundle_command=bundle
+# compdef _bundle_command=bundle
 
 function b() {
   say-and-execute _bundle_command $* | grep -v 'Using '
