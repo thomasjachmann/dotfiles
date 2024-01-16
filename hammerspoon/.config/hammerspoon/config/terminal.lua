13bd001cbd49774f6cfb650f1f1ff4c61d1cd10a
Thomas Jachmann <self@thomasjachmann.com>
2024-01-16 11:33:55 +0100

    hammerspoon: Move terminal to own config
    I want to group the mappings/configs by function and separate global and
    machine specific mappings/configs.

---
+++ hammerspoon/.config/hammerspoon/config/terminal.lua
@@ -0,0 +1,20 @@
+local hyper = require "lib.hyper"
+local tmux = require "apps.tmux"
+local kitty = require "apps.kitty"
+local apps = require "lib.apps"
+
+local mappings = {
+  e = tmux.launch("nvim", {noToggle=true}), -- e for editor
+
+  q = kitty.focusOrLaunch{title="taskwarrior-tui", cmd="taskwarrior-tui"},
+  [{"shift", "q"}] = kitty.focusOrLaunch{title="taskwarrior", cmd="tasksh", text="next\n"},
+
+  r = tmux.activate("ruby"), -- r anything that's running ruby (rails server, guard, formeman, rails console)
+
+  t = apps.launch("net.kovidgoyal.kitty", {noToggle=true}), -- t for terminal
+  [{"shift", "t"}] = kitty.focusOrLaunch(), -- always a new terminal window
+  [{"alt", "t"}] = kitty.focusOrLaunch{shell=true}, -- terminal without tmux
+
+  x = kitty.focusOrLaunch{title="vimwiki", cmd="nvim -c 'call VimwikiOpen()'"},
+}
+hyper.bindAll(mappings)
--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -4,25 +4,17 @@ hs.hotkey.setLogLevel("warning")
 local apps = require "lib.apps"
 local hyper = require "lib.hyper"
 
-local tmux = require "apps.tmux"
-local kitty = require "apps.kitty"
 
 require "config.window_management"
 require "config.default_apps"
+require "config.terminal"
 
 local hyperMappings = {
 
 
   d = apps.launch("Calendar"), -- d for dates
-  e = tmux.launch("nvim", {noToggle=true}), -- e for editor
   m = apps.launch("Thunderbird"),
-  q = kitty.focusOrLaunch{title="taskwarrior-tui", cmd="taskwarrior-tui"},
-  [{"shift", "q"}] = kitty.focusOrLaunch{title="taskwarrior", cmd="tasksh", text="next\n"},
-  r = tmux.activate("ruby"), -- r anything that's running ruby (rails server, guard, formeman, rails console)
   s = apps.launch("Skype"),
-  [{"shift", "t"}] = kitty.focusOrLaunch(),
-  [{"alt", "t"}] = kitty.focusOrLaunch{shell=true},
-  t = apps.launch("net.kovidgoyal.kitty", {noToggle=true}), -- t for terminal
   [{"shift", "w"}] = function()
     hs.wifi.setPower(hs.wifi.currentNetwork() == nil)
   end,
