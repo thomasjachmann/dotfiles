ad04f47b5024cbe2d788c4715e811ad7a6532a42
Thomas Jachmann <self@thomasjachmann.com>
2024-01-16 13:19:28 +0100

    hammerspoon: Remove machine specific mappings (!)
    Breaking!!
    
    To keep these, move them to config/local_xyz.lua.

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -1,6 +1,5 @@
 hs.hotkey.setLogLevel("warning")
 
-local apps = require "lib.apps"
 local local_files = require "lib.local_files"
 local hyper = require "lib.hyper"
 
@@ -12,15 +11,9 @@ require "config.project_chooser"
 local_files.each("config", "local_")
 
 local mappings = {
-  d = apps.launch("Calendar"), -- d for dates
-  m = apps.launch("Thunderbird"),
-  s = apps.launch("Skype"),
   [{"shift", "w"}] = function()
     hs.wifi.setPower(hs.wifi.currentNetwork() == nil)
   end,
-  y = apps.launch("Rambox"), -- y for whatever, this space gets crowded!! y is next to s which has a similar function, I need multikey shortcuts!
-  z = apps.launch("zoom.us", {forceToggle=true}),
-
 
   escape = function()
     hs.openConsole()
