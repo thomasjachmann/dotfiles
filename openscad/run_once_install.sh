0a0c93dd29f8af3a4a725d16859767b24e0c29c3
Thomas Jachmann <self@thomasjachmann.com>
2025-02-21 23:54:15 +0100

    openscad: Use snapshot

--- openscad/run_once_install.sh
+++ openscad/run_once_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 brew bundle --no-lock --file=/dev/stdin <<EOB
-cask "openscad"
+cask "openscad@snapshot"
 
 # The bin/openscad symlink is needed since https://github.com/Homebrew/homebrew-cask/pull/87720
 EOB
