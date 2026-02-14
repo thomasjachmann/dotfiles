370580e5e307bb62acd24a3657c4fe65a4fd3ef4
Thomas Jachmann <thomas.jachmann@sapiens.com>
2026-02-14 19:55:17 +0100

    mise: Prevent python warnings (workaround)

--- mise/private_dot_config/zsh/dot_upgrade.d/mise.zsh
+++ mise/private_dot_config/zsh/dot_upgrade.d/mise.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+mise cache prune # Or do we need `mise cache clear`? This prevents warnings about not being able to update the python plugin.
 mise plugins update
 mise ls --outdated
 if read -qs "CHOICE?Should I run \`mise upgrade\` now? (y/N) "; then
