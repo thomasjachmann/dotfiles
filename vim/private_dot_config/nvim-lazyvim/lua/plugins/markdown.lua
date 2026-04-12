a98d740f16e9f7b082a54423caa5ea35f79e677e
Thomas Jachmann <self@thomasjachmann.com>
2026-04-12 14:06:50 +0200

    vim: Add markdown table editing plugin
    Also refactor config files to move all markdown specific stuff into one
    file. Makes extension/switching plugins easier.

---
+++ vim/private_dot_config/nvim-lazyvim/lua/plugins/markdown.lua
@@ -0,0 +1,29 @@
+return {
+  { -- extend lazyvim's markdown plugin configuration
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      heading = {
+        sign = true,
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+        -- signs = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+        width = "block",
+      },
+      code = {
+        sign = true,
+        width = "full",
+        language_icon = true,
+        border = "thin",
+      },
+      checkbox = {
+        enabled = true,
+      },
+      pipe_table = {
+        border_enabled = false, -- disable top bottom borders for less wiggle
+      },
+    },
+  },
+  { -- add plugin to edit (add/modify/format) markdown tables
+    "SCJangra/table-nvim",
+    ft = "markdown",
+  },
+}
